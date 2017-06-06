b1bbdeb313dfa5c8786b745434310616b8d29bef
stsupertam <supmantam@gmail.com>
2017-06-06 16:57:07 +0700

    add content page

---
+++ my-project/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Content from '@/components/Content'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: Content
+    }
+  ]
+})
